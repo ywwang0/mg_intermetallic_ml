75e78b7a26704315b1fb106a3d44f7b4421940cd
Yaowei Wang <43977524+ywwang0@users.noreply.github.com>
2021-04-19 14:57:03 +0800

    Update vornoi_volumn_and_cn.py

--- vornoi_volumn_and_cn.py
+++ vornoi_volumn_and_cn.py
@@ -8,7 +8,7 @@ Input: pymatgen structure class
 Output: csv file containing all necessary dictionary(c dictionary)
 History: 
 04/18/2021: Created
-
+04/19/2021: Add 求配位数中的元素种类
 To be chieve:
 None
 *******************************************************************
@@ -17,6 +17,7 @@ None
 from pymatgen.analysis.local_env import VoronoiNN
 from pymatgen.core.structure import Structure
 import pandas as pd
+from collections import Counter
 
 '''
 tol (float) – tolerance parameter for near-neighbor finding. Faces that are smaller than tol fraction of the largest face are not included in the tessellation. (default: 0).
@@ -31,13 +32,25 @@ cn = VoronoiNN(tol=0, targets=None, cutoff=13.0, allow_pathological=True,
                weight='solid_angle', extra_nn_info=True, compute_adj_neighbors=True)
 
 # 更改成当前文件下需要分析的文件
-struct = Structure.from_file("Al-111-0001.cif")
+struct = Structure.from_file("mgf2.vasp")
+
+'''
+solid_angle - Solid angle subtended(对向的) by face
+angle_normalized - Solid angle normalized such that the faces with the largest
+area - Area of the facet
+face_dist - Distance between site n and the facet
+volume - Volume of Voronoi cell for this face
+n_verts - Number of vertices(顶点数) on the facet
+'''
+print(f"结构的原子数为：{len(cn.get_all_nn_info(struct))}")
+print(f"结构中第一个原子的配位数为：{len(cn.get_all_nn_info(struct)[1])}")
 
 atom_index = []
 ele = []
 nn = []
 pos = []
 vol = []
+nn_ele = []
 
 # 保存原子序数，元素符号，元素坐标和配位数
 for i in range(len(struct)):
@@ -58,13 +71,25 @@ for i in range(len(struct)):
         sum = sum + dic[key]['volume']
     vol.append(sum)
 
+nn_ele = []
+# 求配位数中的元素种类
+for i in range(len(struct)):
+    elments = []
+    for a in cn.get_all_voronoi_polyhedra(struct)[i]:
+        info = cn.get_all_voronoi_polyhedra(struct)[i][a]['site']
+        elment = str(info).split()[-1]
+        elments.append(elment)
+
+    nn_ele.append(dict(Counter(elments)))
+    
 c = {
     "atom_index":atom_index,
-    'ele':ele,
+    'element':ele,
     'nn':nn,
     'vor_volumn':vol,
+    'cn_ele':nn_ele,
     'position':pos
 }
 
 df = pd.DataFrame(c)
-df.to_csv('vornoi_output.csv',index=None)
+df.to_csv('test.csv',index=None)
